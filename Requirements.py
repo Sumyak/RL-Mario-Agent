d52aaf6b0a162f401d8e9dbbf61d14620c36e34b
Sumyak <94915918+Sumyak@users.noreply.github.com>
2022-06-30 16:37:02 +0530

    Create Requirements.py

---
+++ Requirements.py
@@ -0,0 +1,7 @@
+!pip install nes-py==0.2.6
+!pip install gym-super-mario-bros
+!apt-get update
+!apt-get install ffmpeg libsm6 libxext6  -y
+!apt install -y libgl1-mesa-glx
+!pip install opencv-python
+
