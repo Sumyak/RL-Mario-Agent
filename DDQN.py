ff38c38689789dcfd4016b287c28ebffd3943a88
Sumyak <94915918+Sumyak@users.noreply.github.com>
2022-06-30 17:27:20 +0530

    Update DDQN.py

--- DDQN.py
+++ DDQN.py
@@ -9,11 +9,10 @@ import torch
 import torch.nn as nn
 import random
 import gym_super_mario_bros
-from nes_py import wrappers
-#from nes_py.wrappers import JoypadSpace
+from nes_py.wrappers import JoypadSpace
 from tqdm import tqdm
 import pickle 
-#from gym_super_mario_bros.actions import RIGHT_ONLY
+from gym_super_mario_bros.actions import RIGHT_ONLY
 import gym
 import numpy as np
 import collections 
