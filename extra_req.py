ee47e195362c2588a6fc36e65b478f7f6d6a1edc
Sumyak <94915918+Sumyak@users.noreply.github.com>
2022-06-30 17:15:22 +0530

    Update extra_req.py

--- extra_req.py
+++ extra_req.py
@@ -1,3 +1,3 @@
-apt-get update
-apt-get install ffmpeg libsm6 libxext6  -y
-apt install -y libgl1-mesa-glx
+!apt-get update
+!apt-get install ffmpeg libsm6 libxext6  -y
+!apt install -y libgl1-mesa-glx
