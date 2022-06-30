c61ab13ebd1c7604440f6c991abf5d8638c65412
Sumyak <94915918+Sumyak@users.noreply.github.com>
2022-06-30 16:41:47 +0530

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -1,5 +1,5 @@
-!pip install nes-py==0.2.6
-!pip install gym-super-mario-bros
+nes-py==0.2.6
+gym-super-mario-bros
 !apt-get update
 !apt-get install ffmpeg libsm6 libxext6  -y
 !apt install -y libgl1-mesa-glx
